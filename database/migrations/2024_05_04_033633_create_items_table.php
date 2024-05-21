c649f633c5ddd41f531469a6cecd20af716826ab
iwans123 <bimasetyawan565@gmail.com>
2024-05-21 16:50:06 +0700

    change item table

--- database/migrations/2024_05_04_033633_create_items_table.php
+++ database/migrations/2024_05_04_033633_create_items_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('items', function (Blueprint $table) {
             $table->id();
-            $table->integer('code')->unique();
+            $table->string('code')->unique();
             $table->string('name');
             $table->string('part_number')->unique()->nullable();;
             $table->foreignId('category_id');
