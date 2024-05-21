e619df7296d0555cb4d9749ac193580dfe6bfb44
iwans123 <bimasetyawan565@gmail.com>
2024-05-21 16:38:31 +0700

    fix chart dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -18,21 +18,74 @@ class DashboardController extends Controller
 
         $totalMonth = Order::whereMonth('created_at', $currentMonth)->whereYear('created_at', $currentYear)->sum('total_price');
         $totalToday = Order::whereDate('created_at', Carbon::today())->sum('total_price');
-        return view('dashboard', compact('totalToday', 'totalMonth'));
+
+        $totalFirst = Order::whereHas('stock', function ($query) {
+            $query->where('position_id', '1');
+        })
+            ->whereMonth('created_at', $currentMonth)
+            ->whereYear('created_at', $currentYear)->sum('total_price');
+        $totalSecond = Order::whereHas('stock', function ($query) {
+            $query->where('position_id', '2');
+        })
+            ->whereMonth('created_at', $currentMonth)
+            ->whereYear('created_at', $currentYear)->sum('total_price');
+
+        return view('dashboard', compact('totalToday', 'totalMonth', 'totalFirst', 'totalSecond', 'currentMonth'));
     }
 
     public function chart()
     {
         $currentMonth = Carbon::now()->month;
         $currentYear = Carbon::now()->year;
-        $orders = Order::selectRaw('DATE(created_at) as date, SUM(total_price) as totals')->groupBy('date')->whereMonth('created_at', $currentMonth)->whereYear('created_at', $currentYear)->get();
+        // $orders = Order::selectRaw('YEAR(created_at) year, MONTH(created_at) month, SUM(total_price) total')
+        //     ->groupBy('year', 'month')
+        //     ->whereMonth('created_at', $currentMonth)
+        //     ->whereYear('created_at', $currentYear)->get();
+
+
+        $monthlyOrders = Order::selectRaw('DATE_FORMAT(created_at, "%Y-%m") as month, sum(total_price) as total_orders')
+            ->groupBy('month')
+            ->orderBy('month')
+            ->get();
+        // Format data untuk chart
+        $labelsOrder = $monthlyOrders->pluck('month')->map(function ($month) {
+            return Carbon::createFromFormat('Y-m', $month)->format('F Y');
+        });
+
+        $valuesOrder = $monthlyOrders->pluck('total_orders');
+
+
+        $firstShop = Order::selectRaw('DATE(created_at) as date, SUM(total_price) as totals')
+            ->groupBy('date')
+            ->orderBy('date')
+            ->whereMonth('created_at', $currentMonth)
+            ->whereYear('created_at', $currentYear)
+            ->whereHas('stock', function ($query) {
+                $query->where('position_id', '1');
+            })->get();
+
+        $secondShop = Order::selectRaw('DATE(created_at) as date, SUM(total_price) as totals')
+            ->groupBy('date')
+            ->orderBy('date')
+            ->whereMonth('created_at', $currentMonth)
+            ->whereYear('created_at', $currentYear)
+            ->whereHas('stock', function ($query) {
+                $query->where('position_id', '2');
+            })->get();
 
         // dd($orders);
         // $formattedOrders = $orders->map(function ($order) {
         //     $order->totals = formatRupiah($order->totals);
         //     return $order;
         // });
-        return response()->json($orders);
+        // \Log::info('firstShop:', $firstShop->toArray());
+        // \Log::info('Second Shop:', $secondShop->toArray());
+        return response()->json([
+            'shop1' => $firstShop,
+            'shop2' => $secondShop,
+            'labelsOrder' => $labelsOrder,
+            'valuesOrder' => $valuesOrder
+        ]);
     }
     /**
      * Show the form for creating a new resource.
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -19,104 +19,102 @@
         </div>
     </div> --}}
     <div class="row">
-        <div class="col-md-3">
-            <div class="card">
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col mt-0">
-                            <h5 class="card-title">Penjualan</h5>
-                        </div>
+        <div class="col-xl-6 col-xxl-5 d-flex">
+            <div class="w-100">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col mt-0">
+                                        <h5 class="card-title">Sales</h5>
+                                    </div>
 
-                        <div class="col-auto">
-                            <div class="stat text-primary">
-                                <i class="align-middle" data-feather="truck"></i>
+                                    <div class="col-auto">
+                                        <div class="stat text-primary">
+                                            <i class="align-middle" data-feather="truck"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                                <h1 class="mt-1 mb-3">{{ formatRupiah($totalToday, true) }}</h1>
+                                <div class="mb-0">
+                                    {{-- <span class="text-danger"> <i class="mdi mdi-arrow-bottom-right"></i> -3.65% </span> --}}
+                                    <span class="text-muted">{{ \Carbon\Carbon::now()->format('jS F Y') }}</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col mt-0">
+                                        <h5 class="card-title">Total Sales</h5>
+                                    </div>
+
+                                    <div class="col-auto">
+                                        <div class="stat text-primary">
+                                            <i class="align-middle" data-feather="users"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                                <h1 class="mt-1 mb-3">{{ formatRupiah($totalMonth, true) }}</h1>
+                                <div class="mb-0">
+                                    {{-- <span class="text-success"> <i class="mdi mdi-arrow-bottom-right"></i> 5.25% </span> --}}
+                                    <span class="text-muted">{{ \Carbon\Carbon::now()->format('F') }}</span>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <h1 class="mt-1 mb-3">{{ formatRupiah($totalToday, true) }}</h1>
-                    <div class="mb-0">
-                        {{-- <span class="text-danger"> <i class="mdi mdi-arrow-bottom-right"></i> -3.65% </span> --}}
-                        <span class="text-muted">Hari ini</span>
+                    <div class="col-sm-6">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col mt-0">
+                                        <h5 class="card-title">Order Toko 1</h5>
+                                    </div>
+
+                                    <div class="col-auto">
+                                        <div class="stat text-primary">
+                                            <i class="align-middle" data-feather="dollar-sign"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                                <h1 class="mt-1 mb-3">{{ formatRupiah($totalFirst, true) }}</h1>
+                                <div class="mb-0">
+                                    {{-- <span class="text-success"> <i class="mdi mdi-arrow-bottom-right"></i> 6.65% </span> --}}
+                                    <span class="text-muted">{{ \Carbon\Carbon::now()->format('F') }}</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col mt-0">
+                                        <h5 class="card-title">Order Toko 2</h5>
+                                    </div>
+
+                                    <div class="col-auto">
+                                        <div class="stat text-primary">
+                                            <i class="align-middle" data-feather="dollar-sign"></i>
+                                            {{-- <i class="align-middle" data-feather="shopping-cart"></i> --}}
+                                        </div>
+                                    </div>
+                                </div>
+                                <h1 class="mt-1 mb-3">{{ formatRupiah($totalSecond, true) }}</h1>
+                                <div class="mb-0">
+                                    {{-- <span class="text-danger"> <i class="mdi mdi-arrow-bottom-right"></i> -2.25% </span> --}}
+                                    <span class="text-muted">{{ \Carbon\Carbon::now()->format('F') }}</span>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="col-md-3">
-            <div class="card">
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col mt-0">
-                            <h5 class="card-title">Penjualan</h5>
-                        </div>
 
-                        <div class="col-auto">
-                            <div class="stat text-primary">
-                                <i class="align-middle" data-feather="users"></i>
-                            </div>
-                        </div>
-                    </div>
-                    <h1 class="mt-1 mb-3">{{ formatRupiah($totalMonth, true) }}</h1>
-                    <div class="mb-0">
-                        {{-- <span class="text-success"> <i class="mdi mdi-arrow-bottom-right"></i> 5.25% </span> --}}
-                        <span class="text-muted">Dalam satu bulan</span>
-                    </div>
-                </div>
-            </div>
-        </div>
-        {{-- <div class="col-sm-6">
-            <div class="card">
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col mt-0">
-                            <h5 class="card-title">Earnings</h5>
-                        </div>
-
-                        <div class="col-auto">
-                            <div class="stat text-primary">
-                                <i class="align-middle" data-feather="dollar-sign"></i>
-                            </div>
-                        </div>
-                    </div>
-                    <h1 class="mt-1 mb-3">$21.300</h1>
-                    <div class="mb-0">
-                        <span class="text-success"> <i class="mdi mdi-arrow-bottom-right"></i> 6.65% </span>
-                        <span class="text-muted">Since last week</span>
-                    </div>
-                </div>
-            </div>
-            <div class="card">
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col mt-0">
-                            <h5 class="card-title">Orders</h5>
-                        </div>
-
-                        <div class="col-auto">
-                            <div class="stat text-primary">
-                                <i class="align-middle" data-feather="shopping-cart"></i>
-                            </div>
-                        </div>
-                    </div>
-                    <h1 class="mt-1 mb-3">64</h1>
-                    <div class="mb-0">
-                        <span class="text-danger"> <i class="mdi mdi-arrow-bottom-right"></i> -2.25% </span>
-                        <span class="text-muted">Since last week</span>
-                    </div>
-                </div>
-            </div>
-        </div> --}}
-    </div>
-    {{-- <div class="row">
-        <div class="col-xl-3 col-xxl-3 d-flex">
-        </div>
-        <div class="w-100">
-        </div>
-
-        <div class="col-xl-9 col-xxl-9">
+        <div class="col-xl-6 col-xxl-7">
             <div class="card flex-fill w-100">
                 <div class="card-header">
-
-                    <h5 class="card-title mb-0">Recent Movement</h5>
+                    <h5 class="card-title mb-0">Total Order Grafik</h5>
                 </div>
                 <div class="card-body py-3">
                     <div class="chart chart-sm">
@@ -125,13 +123,13 @@
                 </div>
             </div>
         </div>
-    </div> --}}
+    </div>
     <div class="row">
         <div class="col-md-12">
             <div class="card flex-fill w-100">
                 <div class="card-header">
                     <h5 class="card-title">Order Grafik</h5>
-                    <h6 class="card-subtitle text-muted">Total Order</h6>
+                    <h6 class="card-subtitle text-muted">Total Order @hari</h6>
                 </div>
                 <div class="card-body">
                     <div class="chart">
@@ -142,14 +140,26 @@
         </div>
     </div>
 
+    {{-- chart 1 --}}
     <script>
         document.addEventListener("DOMContentLoaded", function() {
             // Line chart
             fetch('/dashboard/chart')
                 .then(response => response.json())
                 .then(data => {
-                    const labels = data.map(orders => orders.date);
-                    const totals = data.map(orders => orders.totals);
+                    // console.log(data);
+
+                    // if (!data.shop1 || !data.shop2) {
+                    //     console.error("Data shop1 or shop2 is undefined.");
+                    //     return;
+                    // }
+
+                    const labelsShop1 = data.shop1.map(order => order.date);
+                    const totalsShop1 = data.shop1.map(order => order.totals);
+                    const labelsShop2 = data.shop2.map(order => order.date);
+                    const totalsShop2 = data.shop2.map(order => order.totals);
+
+                    const labels = [...new Set([...labelsShop1, ...labelsShop2])];
 
                     var ctx = document.getElementById("chartjs-line").getContext("2d");
                     var gradient = ctx.createLinearGradient(0, 0, 0, 225);
@@ -165,8 +175,22 @@
                                 fill: true,
                                 backgroundColor: gradient,
                                 borderColor: window.theme.primary,
-                                data: totals
-                            }, ]
+                                data: labels.map(label => {
+                                    const order = data.shop1.find(order => order
+                                        .date === label);
+                                    return order ? order.totals : 0;
+                                })
+                            }, {
+                                label: "Shop 2 Sales",
+                                fill: true,
+                                backgroundColor: "rgba(255, 99, 132, 0.1)",
+                                borderColor: "rgba(255, 99, 132, 1)",
+                                data: labels.map(label => {
+                                    const order = data.shop2.find(order => order
+                                        .date === label);
+                                    return order ? order.totals : 0;
+                                })
+                            }]
                         },
                         options: {
                             maintainAspectRatio: false,
@@ -205,44 +229,57 @@
                             }
                         }
                     });
-                });
-        });
-    </script>
 
-    {{-- <div style="width: 75%; margin: auto;">
-        <canvas id="transactionsChart"></canvas>
-    </div>
-
-    <script>
-        document.addEventListener('DOMContentLoaded', function() {
-            fetch('/dashboard/chart')
-                .then(response => response.json())
-                .then(data => {
-                    const labels = data.map(orders => orders.date);
-                    const totals = data.map(orders => orders.total);
-
-                    const ctx = document.getElementById('transactionsChart').getContext('2d');
-                    new Chart(ctx, {
-                        type: 'line',
+                    new Chart(document.getElementById("chartjs-dashboard-line"), {
+                        type: "line",
                         data: {
-                            labels: labels,
+                            labels: data.labelsOrder,
                             datasets: [{
-                                label: 'Total Transactions',
-                                data: '12500',
-                                borderColor: 'rgba(75, 192, 192, 1)',
-                                backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                                borderWidth: 1
-                            }]
+                                label: "Sales ($)",
+                                fill: true,
+                                backgroundColor: "transparent",
+                                borderColor: window.theme.primary,
+                                data: data.valuesOrder
+                            }, ]
                         },
                         options: {
-                            scales: {
-                                y: {
-                                    beginAtZero: true
+                            maintainAspectRatio: false,
+                            legend: {
+                                display: false
+                            },
+                            tooltips: {
+                                intersect: false
+                            },
+                            hover: {
+                                intersect: true
+                            },
+                            plugins: {
+                                filler: {
+                                    propagate: false
                                 }
+                            },
+                            scales: {
+                                xAxes: [{
+                                    reverse: true,
+                                    gridLines: {
+                                        color: "rgba(0,0,0,0.05)"
+                                    }
+                                }],
+                                yAxes: [{
+                                    ticks: {
+                                        stepSize: 1000000
+                                    },
+                                    display: true,
+                                    borderDash: [5, 5],
+                                    gridLines: {
+                                        color: "rgba(0,0,0,0)",
+                                        fontColor: "#fff"
+                                    }
+                                }]
                             }
                         }
                     });
                 });
         });
-    </script> --}}
+    </script>
 @endsection
